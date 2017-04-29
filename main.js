4d32aacb299596649f8f92fbf5e70e364159834c
hockey4life63 <Amelancon68@gmail.com>
2017-04-29 13:20:21 -0700

    merged

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-//$(document).ready(function(){
-    var calc ={
-        firstNumber : "",
-        secondNumber : "",
-        total: "",
-        operatorStored : "",
-        state : 0,
-        res : 0,
-        updateDisplay: function(){
 
-        },
-        clearData: function(){
-
-        },
-        calculateTotal: function() {
-            if (this.state === 1) {
-                var firstNum = parseInt(this.firstNumber);
-                var secondNUm = parseInt(this.secondNumber);
-                var oper;
-                switch(this.operatorStored) {
-                    case 'plus':
-                        this.total = firstNum + secondNUm;
-                        break;
-                    case 'minus':
-                        this.total = firstNum - secondNUm;
-                        break;
-                    case 'times':
-                        this.total = firstNum * secondNUm;
-                        break;
-                    case 'divide':
-                        this.total = firstNum / secondNUm;
-                        break;
-                    case 'power':
-                        this.total = Math.pow(firstNum, secondNUm);
-                        break;   
-                }
-            }
-        }
-    };
-//});
-=======
 // $(document).ready(function(){
     var calc = {
         firstNumber : "",
@@ -85,6 +44,30 @@
             $("#operator").empty();
             $("#result").empty();
 
+        },
+        calculateTotal: function() {
+            if (this.state === 1) {
+                var firstNum = parseInt(this.firstNumber);
+                var secondNUm = parseInt(this.secondNumber);
+                var oper;
+                switch(this.operatorStored) {
+                    case 'plus':
+                        this.total = firstNum + secondNUm;
+                        break;
+                    case 'minus':
+                        this.total = firstNum - secondNUm;
+                        break;
+                    case 'times':
+                        this.total = firstNum * secondNUm;
+                        break;
+                    case 'divide':
+                        this.total = firstNum / secondNUm;
+                        break;
+                    case 'power':
+                        this.total = Math.pow(firstNum, secondNUm);
+                        break;   
+                }
+            }
         }
     };
 
