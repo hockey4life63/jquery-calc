b1c3da7350c28a859b251ae97438fc53cec5315c
Leeland Miller <leelandm@Leelands-MacBook-Pro.local>
2017-04-29 12:34:28 -0700

    calculateTotal method

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
-$(document).ready(function(){
+//$(document).ready(function(){
     var calc ={
-        firstNumber : "";
-        secondNmber : "";
-        operatorStored : "";
-        state : 0;
-        res : 0;
-        function updateDisplay(){
+        firstNumber : "",
+        secondNumber : "",
+        total: "",
+        operatorStored : "",
+        state : 0,
+        res : 0,
+        updateDisplay: function(){
 
-        };
-        function clearData(){
+        },
+        clearData: function(){
 
-        };
+        },
+        calculateTotal: function() {
+            if (this.state === 1) {
+                var firstNum = parseInt(this.firstNumber);
+                var secondNUm = parseInt(this.secondNumber);
+                var oper;
+                switch(this.operatorStored) {
+                    case 'plus':
+                        this.total = firstNum + secondNUm;
+                        break;
+                    case 'minus':
+                        this.total = firstNum - secondNUm;
+                        break;
+                    case 'times':
+                        this.total = firstNum * secondNUm;
+                        break;
+                    case 'divide':
+                        this.total = firstNum / secondNUm;
+                        break;
+                    case 'power':
+                        this.total = Math.pow(firstNum, secondNUm);
+                        break;   
+                }
+            }
+        }
     };
-
-
-
-});
+//});
