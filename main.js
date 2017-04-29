08c5b806f34be9a683342da0ea3f9a7cb13dee06
hockey4life63 <Amelancon68@gmail.com>
2017-04-29 11:54:05 -0700

    added js file

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var calc ={
+        firstNumber : "";
+        secondNmber : "";
+        operatorStored : "";
+        state : 0;
+        res : 0;
+        function updateDisplay(){
+
+        };
+        function clearData(){
+
+        };
+    };
+
+
+
+});
